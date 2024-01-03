3acac3b52dd96e40cbfe271664cacddec0cf4bf0
Vish <vishaylinparshotam@gmail.com>
2024-01-03 08:51:52 -0800

    error fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Print alphabets in lower case , but exclude q and e
-for letter in range(97,122):
+for letter in range(97,123):
 	if chr(letter) == 'e' or chr(letter) == 'q':
 		continue
 	print(chr(letter).format(letter), end = '')
