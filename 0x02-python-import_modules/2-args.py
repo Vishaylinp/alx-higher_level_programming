a3caf2a211c69cd89f452d8b5c850f5a34c46d0c
Vish <vishaylinparshotam@gmail.com>
2024-01-08 01:35:05 -0800

    handling arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,12 +4,11 @@ if __name__ == "__main__":
 	import sys
 
 	count = len(sys.argv) - 1
-
 	if count == 0:
-		print("0 arguments")
+		print("0 arguments:")
 	elif count == 1:
-		print("1 arguements")
+		print("1 argument:")
 	else:
-		print("{} arguements :".format(count))
+		print("{} arguments:".format(count))
 	for c in range(count):
-		print("{}: {}".format(c+1, sys.argv[c+1]))
+		print("{}: {}".format(c + 1, sys.argv[c + 1]))
