5d09c450285a8659b1347f8639288835c5e30a37
Vish <vishaylinparshotam@gmail.com>
2024-01-25 04:38:12 -0800

    area of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""A square class"""
+
+
+class Square:
+    """A square"""
+
+    def __init__(self, size=0):
+        """Initalisation
+
+        Arg:
+           size: size of a square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Area of square"""
+        return(self.__size * self.__size)
