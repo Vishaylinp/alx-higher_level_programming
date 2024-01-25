5de0eab8416f64bf642432b4d9788447bfbd0af2
Vish <vishaylinparshotam@gmail.com>
2024-01-25 01:32:47 -0800

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""A square class"""
+
+
+class Square:
+    """A square"""
+
+    def __init__(self, size):
+        """initialisation
+        Args:
+             size: size of square
+        """
+        self.__size = size
