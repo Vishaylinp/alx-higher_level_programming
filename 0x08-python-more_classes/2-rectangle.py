9a34c7bf00b5439c4e42187e45b845c63ea22eda
Vish <vishaylinparshotam@gmail.com>
2024-01-29 23:59:46 -0800

    error fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -44,10 +44,10 @@ class Rectangle:
 
     def area(self):
         """Area of rectsngle"""
-        return(self.__height * self.__width)
+        return (self.__height * self.__width)
 
     def perimeter(self):
         """Perimeter of rectangle"""
         if self.__width == 0 or self.__height == 0:
             return (0)
-        return((2 * self.__width) + (2 * self.__height))
+        return ((2 * self.__width) + (2 * self.__height))
