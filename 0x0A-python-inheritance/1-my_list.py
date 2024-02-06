a2f8a6beb19439fe6ac5b0e53873c56d1d30c7f4
Vish <vishaylinparshotam@gmail.com>
2024-02-06 05:27:03 -0800

    error fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,6 @@
 class MyList(list):
     """print the list"""
 
-    def __init__(self):
-        """initilaisation"""
-        super().__init__()
-
     def print_sorted(self):
         """print in ascending"""
         print(sorted(self))
