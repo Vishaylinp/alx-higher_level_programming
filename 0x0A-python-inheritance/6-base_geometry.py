b7f84c1ddc2ff400010dbb06eda5310876db966c
Vish <vishaylinparshotam@gmail.com>
2024-02-06 06:38:56 -0800

    exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Empty class"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+
+    def area(self):
+        """area with exception"""
+        raise Exception("area() is not implemented")
