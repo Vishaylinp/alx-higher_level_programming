f2f9da87e2a7c9143ea2b31c96a8a8082dd36d41
Vish <vishaylinparshotam@gmail.com>
2024-03-13 07:47:25 -0700

    create table and records

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- Create a new table names second_table and add records
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+insert INTO second_table (id, name, score) VALUES (1, "John", 10);
+insert INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+insert INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+insert INTO second_table (id, name, score) VALUES (4, "George", 8);
