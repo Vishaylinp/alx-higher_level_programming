4381f35786fa294d21b7d98a7b45488c71d50dbf
Vishaylinp <vishaylinparshotam@gmail.com>
2024-04-10 01:02:36 -0700

    error fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const argc = process.argv.length;
-if (argc === 2) {
+let argu = process.argv.length;
+if (argu === 2) {
   console.log('No argument');
-} else if (argc === 3) {
+} else if (argu === 3) {
   console.log('Argument found');
 } else {
   console.log('Argument found');
