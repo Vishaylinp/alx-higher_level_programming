c426c912362c3bde323aee6aafae164809704c74
Vishaylinp <vishaylinparshotam@gmail.com>
2024-04-10 00:42:49 -0700

    corrections

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-function add(a, b) {
+function add (a, b) {
   const result = a + b;
   console.log(result);
 }
-add(Number(process.argv[2]), Number(process.argv[3]))
+add(Number(process.argv[2]), Number(process.argv[3]));
